e07b817c77ab257d87d08d0b7f8973d150ffbc81
AndreParkh <tecon@paa1.teconpc.local>
2023-03-12 19:41:01 +0300

    react Headen, FindForm

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import React from 'react';
+import { Header } from './components/Header'
+import { FindForm } from './components/FindForm';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="screen">
+		<Header/>
+		<div class="container">
+			<div class="column">
+				<div class="column__left">
+					<FindForm/>
+				</div>
+				<div class="column__right">
+
+				</div>
+			</div>
+		</div>
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <div className="header">
+      <h1 className="header__title">Simple Hotel Check</h1>
+      <div className="header__logout">
+        <div className="logout__text">Выйти</div>
+        <div className="logout__img"></div>
+      </div>
+    </div>
+  );
+};
+
+export { Header };
